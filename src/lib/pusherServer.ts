cf5a1f10063e25ff9f54a87baa15356be376c30a
oliiaroleksandr <sashaoliyar@gmail.com>
2025-06-19 21:14:02 +0300

    restart vercel

--- src/lib/pusherServer.ts
+++ src/lib/pusherServer.ts
@@ -9,6 +9,7 @@ export const pusherServer = new PusherServer({
   useTLS: true,
 });
 
+
 export const beamsServer = new PushNotificationsServer({
   instanceId: "f199e784-472b-4bd6-a7fe-d908e3a48b04",
   secretKey: "79933E6CA61C2A8941BC60962465B63D1ED4A63EE80E3D11E21065928EF46FEA",
